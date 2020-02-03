b33e5005bd7ac113355ba5d3a1628efe2e3cdfe0
jungon <j017.kim@gmail.com>
2020-02-04 00:17:03 +0900

    Computed

---
+++ src/api/User/computed.js
@@ -0,0 +1,39 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullname: parent => `${parent.lastname} ${parent.firstname}`,
+    isFollowing: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({
+          AND: [{ id: parentId }, { followers_some: { id_in: [user.id] } }]
+        });
+      } catch {
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  },
+  Post: {
+    isLiked: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id
+            }
+          },
+          { post: { id: parentId } }
+        ]
+      });
+    }
+  }
+};
