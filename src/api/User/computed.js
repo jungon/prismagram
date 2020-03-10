5c3f6fd2d131a372277d73045210454bc06ff65e
jungon <j017.kim@gmail.com>
2020-03-11 06:32:58 +0900

    modify user computed

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -8,8 +8,37 @@ export default {
     likes: ({ id }) => prisma.user({ id }).likes(),
     comments: ({ id }) => prisma.user({ id }).comments(),
     rooms: ({ id }) => prisma.user({ id }).rooms(),
+    postsCount: ({ id }) =>
+      prisma
+        .postsConnection({ where: { id } })
+        .aggregate()
+        .count(),
     followingCount: ({ id }) =>
-      prisma.usersConnection({ where: { followers_some: { id } } }).aggregate()
+      prisma
+        .usersConnection({ where: { followers_some: { id } } })
+        .aggregate()
+        .count(),
+    followersCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { following_some: { id } } })
+        .aggregate.count(),
+    fullname: parent => `${parent.firstname} ${parent.lastname}`,
+    isFollowing: (parent, _, { request }) => {
+      const { users } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({
+          AND: [{ id: user.id }, { following_some: { id: parentId } }]
+        });
+      } catch {
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
   },
   Post: {
     user: ({ id }) => prisma.post({ id }).user(),
